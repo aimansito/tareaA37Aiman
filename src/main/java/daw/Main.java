a5e9283ac2ad81fcc39b56f0dde7053615cb9691
Aiman <ahardao1001@g.educaand.es>
2024-05-02 12:44:39 +0200

    c2

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -1,16 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.tareaa37aiman;
+package daw;
 
 /**
  *
  * @author aiman
  */
-public class TareaA37Aiman {
-
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Formulario f1 = new Formulario();
     }
 }
