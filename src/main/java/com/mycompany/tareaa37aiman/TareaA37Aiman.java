96d34e3c9fba11105f838d67b9068293632c30cb
Aiman <ahardao1001@g.educaand.es>
2024-05-02 10:54:04 +0200

    c1

---
+++ src/main/java/com/mycompany/tareaa37aiman/TareaA37Aiman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareaa37aiman;
+
+/**
+ *
+ * @author aiman
+ */
+public class TareaA37Aiman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
